0f3fe8ef2eb26fb7e40d209dc4ff31a75c84283f
YorikMol <yakimenko3942@gmail.com>
2023-08-16 19:20:30 +0300

    Added my day screen,made app edge to edge

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,23 +5,44 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,20 +50,26 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             val navController = rememberNavController()
 
             NavHost(navController, startDestination = "mainScreen") {
                 composable("mainScreen") { MainScreen(navController) }
-                composable("myDay") { MyDay() }
+                composable("myDay") { MyDay(navController) }
                 composable("tasks") { Tasks() }
             }
         }
@@ -54,7 +81,9 @@ fun MainScreen(navController: NavHostController) {
     Column(
         Modifier
             .fillMaxSize()
-            .background(Color.Black), horizontalAlignment = Alignment.Start
+            .background(Color.Black)
+            .navigationBarsPadding()
+            .statusBarsPadding()
     ) {
         Row(Modifier.padding(top = 8.dp, start = 8.dp)) {
             Image(
@@ -106,7 +135,7 @@ fun MainScreen(navController: NavHostController) {
             )
         }
 
-        Divider(thickness = 1.dp, color = Color.White)
+        HorizontalDivider(thickness = 1.dp, color = Color.White)
 
         val textList = listOf(
             "Item 1",
@@ -141,6 +170,7 @@ fun MainScreen(navController: NavHostController) {
             Modifier
                 .weight(1f)
                 .padding(8.dp)
+                .fillMaxWidth()
         ) {
             items(textList.size) { item ->
                 Text(
@@ -171,9 +201,145 @@ fun MainScreen(navController: NavHostController) {
     }
 }
 
+
 @Composable
-fun MyDay() {
-    Text("My day")
+fun MyDay(navController: NavHostController) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    val calendar = Calendar.getInstance()
+    val dateFormat = SimpleDateFormat("EEEE, MMMM d", Locale.getDefault())
+    val formattedDate = dateFormat.format(calendar.time)
+
+    Image(
+        painter = painterResource(id = R.drawable.background),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+    )
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.statusBars)
+    ) {
+        Row {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(Icons.Default.ArrowBack, tint = Color(0xFFfefafa), contentDescription = null)
+            }
+            Column {
+                Text(text = "My Day", color = Color(0xFFfefafa), fontSize = 22.sp)
+                Text(text = formattedDate, color = Color(0xFFfefafa), fontSize = 16.sp)
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            IconButton(onClick = { expanded = true }) {
+                Icon(Icons.Default.MoreVert, contentDescription = null, tint = Color(0xFFfefafa))
+                DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Change Background") },
+                        onClick = { /*TODO*/ },
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_photo_24),
+                                contentDescription = null
+                            )
+                        })
+                }
+            }
+        }
+        val textList = listOf(
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 1",
+            "Item 2",
+            "Item 3"
+        )
+
+        Box(Modifier.fillMaxSize()) {
+            LazyColumn(
+                Modifier
+                    .fillMaxWidth()
+
+            ) {
+                items(textList.size) { item ->
+                    Card(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(top = 3.dp)
+                            , colors = CardDefaults.cardColors(Color(0xFF212121))
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically){
+                            Checkbox(checked = false, onCheckedChange ={},Modifier.padding(10.dp) )
+                            Text(
+                                text = textList[item],
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+            }
+            FloatingActionButton(
+                onClick = {},
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .navigationBarsPadding()
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_add_24),
+                    contentDescription = null
+                )
+            }
+        }
+    }
 }
 
 @Composable
